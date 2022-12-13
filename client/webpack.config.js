feda9967b550158d5cc4f5e8e62485ebc6ec62d7
nidhi <inidhi99@gmail.com>
2022-12-12 23:23:47 -0500

    added commetns and manifest.json to client webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,36 +8,58 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
+    // devtool: "none",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      //html 
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "JATE",
       }),
+
+      // manifest
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
+      }),
+
+      // manifest.json file 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor with offline development and testing",
+        background_color: "#2B3A55",
+        theme_color: "#CE7777",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
       }),
     ],
 
     module: {
       rules: [
         {
-          // CSS loaders
+          // css loader
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
         },
-        // babel
         {
+          // babel loader
           test: /\.m?js$/,
           exclude: /node_modules|/,
           use: {
