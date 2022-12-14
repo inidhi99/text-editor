9b91c1e48c7b44135e7242a17a6b81341039fd40
nidhi <inidhi99@gmail.com>
2022-12-13 23:29:01 -0500

    // TODO: Implement a click event handler on the  element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,8 +4,23 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+     return;
+    }
+  
+    // Show prompt
+    promptEvent.prompt();
+    
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+  });
 
 // Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
